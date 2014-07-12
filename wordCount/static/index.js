ead4d88f006ed4943048dcc5ceb1d4325e849ab4
Daiki Aminaka <1991.daiki@gmail.com>
2014-07-12 23:31:15 +0900

    added counter of line and paragraph.

--- wordCount/static/index.js
+++ wordCount/static/index.js
@@ -1,8 +1,32 @@
 var len = 0;
 function ShowLength( str ) {
-    document.getElementById("inputlength").innerHTML = str.length + "文字";
-//    document.getElementById("length").value = str.length + "文字";
+    var strArray = str.split("\n");
+    var parNum = getParagraphNum(strArray);
+    var lineNum = getLineNum(strArray);
+    document.getElementById("inputlength").innerHTML = str.length-lineNum + " word";
+    document.getElementById("line").innerHTML = lineNum + " line";
+    document.getElementById("paragraph").innerHTML = parNum + " paragraph";
     len = str.length
+}
+
+function getLineNum(strArray){
+    strArray.sort();
+    var i = 0;
+    while(strArray[i] == ""){i++;console.log(strArray[i]);}
+    return strArray.length - 1 - i;
+}
+
+function getParagraphNum(strArray){
+    var i = 0;
+    var ans = 1;
+    if(strArray[0] == ""){ans--;}
+    while(i < strArray.length-1){
+        if(strArray[i] == "" && strArray[i+1] != ""){
+            ans++;
+        }
+        i++;
+    }
+    return ans
 }
 
 function copyLength(){
