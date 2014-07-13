1a24dc46932b9e6dc11e69a07eca841ca1535ca8
Daiki Aminaka <1991.daiki@gmail.com>
2014-07-13 16:43:07 +0900

    fix bugs and added counter for English (there is not language type switch yet)

--- wordCount/static/index.js
+++ wordCount/static/index.js
@@ -2,18 +2,31 @@ var len = 0;
 function ShowLength( str ) {
     var strArray = str.split("\n");
     var parNum = getParagraphNum(strArray);
-    var lineNum = getLineNum(strArray);
-    document.getElementById("inputlength").innerHTML = str.length-lineNum + " word";
+    var blankNum = getBlankNum(strArray);
+    //var engNum = getEngNum(strArray);
+    var lineNum = strArray.length - blankNum;
+    document.getElementById("inputlength").innerHTML = str.length-blankNum-lineNum+1 + " word";
     document.getElementById("line").innerHTML = lineNum + " line";
     document.getElementById("paragraph").innerHTML = parNum + " paragraph";
-    len = str.length
 }
 
-function getLineNum(strArray){
+function getEngNum(strArray){
+    // ^(\n)+$
+    num = 0;
+    for(var i = 0; i < strArray.length; i++){
+        if(strArray[i] != ""){
+            num += strArray[i].split(" ").length;
+        }
+    }
+    return num;
+}
+
+function getBlankNum(strArray){
+    if(strArray.length == 0 && strArray[0] == ""){return 0;}
     strArray.sort();
     var i = 0;
-    while(strArray[i] == ""){i++;console.log(strArray[i]);}
-    return strArray.length - 1 - i;
+    while(strArray[i] == ""){i++;}
+    return i;
 }
 
 function getParagraphNum(strArray){
